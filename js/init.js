96e5225d34239975093ac8b6cb18c706ac1707ec
Leo Maranhão <lmaranhao@gmail.com>
2015-10-01 18:21:49 -0300

    material design before bruno 2

--- js/init.js
+++ js/init.js
@@ -1,7 +1,8 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown();
+    $(".button-collapse").sideNav();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
