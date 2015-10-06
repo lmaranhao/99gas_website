9dcfdff9d6ec5756547551766a27821e6b00ce57
Leo Maranhão <lmaranhao@gmail.com>
2015-10-05 23:06:41 -0300

    css enhancements (ubuntu font, arrows, red divider, dropdown location

--- js/init.js
+++ js/init.js
@@ -1,8 +1,23 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $(".dropdown-button").dropdown();
-    $(".button-collapse").sideNav();
+        $(".dropdown-button").dropdown({
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: false, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: true, // Displays dropdown below the button
+            alignment: 'left' // Displays dropdown with edge aligned to the left of button
+        });
 
-  }); // end of document ready
+        $(".dropdown-button-mobile").dropdown({
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: false, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: true, // Displays dropdown below the button
+            alignment: 'left' // Displays dropdown with edge aligned to the left of button
+        });
+
+        $(".button-collapse").sideNav();
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
