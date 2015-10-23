a302daad9a150d90413d9f11f3ef414fa7ef7a7c
Leo Maranhão <lmaranhao@gmail.com>
2015-10-23 17:57:31 -0300

    removing old js and css files

--- js/init.js
+++ js/init.js
@@ -1,23 +1,8 @@
-(function($) {
-    $(function() {
+(function($){
+  $(function(){
 
-        $(".dropdown-button").dropdown({
-            constrain_width: false, // Does not change width of dropdown to that of the activator
-            hover: false, // Activate on hover
-            gutter: 0, // Spacing from edge
-            belowOrigin: true, // Displays dropdown below the button
-            alignment: 'left' // Displays dropdown with edge aligned to the left of button
-        });
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
 
-        $(".dropdown-button-mobile").dropdown({
-            constrain_width: false, // Does not change width of dropdown to that of the activator
-            hover: false, // Activate on hover
-            gutter: 0, // Spacing from edge
-            belowOrigin: true, // Displays dropdown below the button
-            alignment: 'left' // Displays dropdown with edge aligned to the left of button
-        });
-
-        $(".button-collapse").sideNav();
-
-    }); // end of document ready
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
