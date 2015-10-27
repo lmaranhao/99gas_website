3c6bd8c3d79032d23d65739bd84636c3f34df9a3
Leo Maranhão <lmaranhao@gmail.com>
2015-10-26 21:24:00 -0300

    button page down

--- js/init.js
+++ js/init.js
@@ -4,5 +4,11 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    $('#down-button-link').click(function(){
+      $('html, body').animate({
+        scrollTop: $("#page-2-container").offset().top
+      }, 500);
+      return false;
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
